52fba6019e1717e7ae3416f3582914172012ca13
MG <mg71645@gmail.com>
2015-08-03 14:26:36 +0600

    New Drag & Drop script

---
+++ js/dragndrop.js
@@ -0,0 +1,41 @@
+$(document).ready( function() {
+    
+    var holding = false;
+    
+    $(document).on('mousedown', 'card > handle', function(e) {
+        console.log('holding', this);
+        holding = true;
+        $(this).parent('card').clone().appendTo('main').addClass('floating').css('top', e.pageY).css('left', e.pageX);
+        $(this).parent('card').addClass('current placeholder').children('card');
+    });
+    
+    $('main').mouseup( function(e) {
+        console.log('dropped', this);
+        holding = false;
+        $('card.floating').remove();
+        $('card').removeClass('current placeholder');
+    });
+    
+    $('main').mousemove( function(e) {
+        if ( holding == false ) {
+            return;
+        } else if ( holding == true ) {
+            console.log('moving', this);
+            $('card.floating').css('top', e.pageY).css('left', e.pageX);
+        }
+    });
+    
+    $('card').mouseover( function(e) {
+        e.stopPropagation();
+        if ( holding == false ) {
+            return;
+        } else if ( holding == true ) {
+            console.log('entering', this);
+            if ( $(this).hasClass('placeholder') == false && $(this).hasClass('new') == false && $(this).children().hasClass('placeholder') == false && $(this).parents().hasClass('placeholder') == false ) {
+                $('.placeholder').removeClass('current')/*.clone()*/.appendTo(this).addClass('current');
+                $('.placeholder:not(.current)').remove();
+            }
+        }
+    });
+    
+});
